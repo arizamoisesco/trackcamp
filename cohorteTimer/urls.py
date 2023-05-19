58713e9b11de7c3e0d5ed0cbcff77e70d9dd34ea
Moises Ariza <arizamoises@gmail.com>
2023-05-18 23:09:20 -0500

    Create views for registered and result

--- cohorteTimer/urls.py
+++ cohorteTimer/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cohorteTimer'
 
 urlpatterns = {
-    path('', views.record_time, name='record_time')
+    path('', views.record_time, name='record_time'),
+    path('result/', views.result, name='result')
 }
--- cohorteTimer/views.py
+++ cohorteTimer/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import Teacher, Program, Cohorte
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 from .forms import RegisterForm
 
 def record_time(request):
-
-    form = RegisterForm()     
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('result')
+    else:
+        form = RegisterForm()     
     return render(request, 'register.html', {'form': form})
+
+# función para el calculo de cohorte
+
+# Función para mostrar el resultado en un template
+def result(response):
+    form = RegisterForm()
+    print(form)
+    return render(response, 'result.html', {"form": form})
