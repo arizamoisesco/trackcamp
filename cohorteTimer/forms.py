77f9e970a0c8a4113cb4a7c8a0b64c4f1e89b467
Moises Ariza <arizamoises@gmail.com>
2023-05-31 21:10:10 -0500

    Add functionality about calculate cohorte time and render in template

--- cohorteTimer/forms.py
+++ cohorteTimer/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Cohorte
 
 class RegisterForm(forms.Form):
-    name_cohorte = forms.CharField(max_length=25)
-    name_trainer = forms.CharField(max_length=25)
-    name_cotrainer = forms.CharField(max_length=25)
+    #name_cohorte = forms.CharField(max_length=25)
+    #name_trainer = forms.CharField(max_length=25)
+    #name_cotrainer = forms.CharField(max_length=25)
+    duration_cohorte = forms.IntegerField(label="Duranción de la cohorte")
+    start_cohorte = forms.DateField(label="Fecha de inicio de cohorte")
--- cohorteTimer/views.py
+++ cohorteTimer/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from .forms import RegisterForm
+from django.views.decorators.http import require_POST
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def record_time(request):
     if request.method == "POST":
         form = RegisterForm(data = request.POST)
         
         if form.is_valid():
-            dato = form.cleaned_data['name_cohorte']
-            return HttpResponseRedirect('result')
+            start_date = form.cleaned_data['start_cohorte']
+            cohorte_duration = form.cleaned_data['duration_cohorte']
+            end_time = time_cohorte(start_date, cohorte_duration)
+            print(end_time)
+            #return HttpResponseRedirect('result')
+            return render(request, "register.html", {'end_time':end_time})
     else:
         form = RegisterForm()     
     return render(request, 'register.html', {'form': form})
@@ -20,11 +25,16 @@ def record_time(request):
 # Inputs: Fecha inicio, duracion_cohorte 
 def time_cohorte(start_date, cohorte_duration):
     #Comprobamos que la información ingresada por el usuario tenga la fecha correcta
-    date = datetime.strptime(start_date, "%d/%m/%Y")
+    #date = datetime.strptime(start_date, "%d/%m/%Y")
     # Convertimos los meses ingresados en dias
+    month_days = 20
+    month_days_totales = cohorte_duration * month_days
     # Sumamos los valores para hacer el calculo
+    future_date = start_date + timedelta(days=month_days_totales)
+    return future_date
 
 # Función para mostrar el resultado en un template
+@require_POST
 def result(response):
     form = RegisterForm()
     # print(form)
