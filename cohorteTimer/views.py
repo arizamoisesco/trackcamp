8fe089a537e8a7904dd7007594ed251a1eacd465
Moises Ariza <arizamoises@gmail.com>
2023-06-01 22:49:08 -0500

    Add method for segment festives days about range start and end date specific

--- cohorteTimer/views.py
+++ cohorteTimer/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .forms import RegisterForm
 from django.views.decorators.http import require_POST
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+import holidays
 
 def record_time(request):
     if request.method == "POST":
@@ -14,7 +15,10 @@ def record_time(request):
             start_date = form.cleaned_data['start_cohorte']
             cohorte_duration = form.cleaned_data['duration_cohorte']
             end_time = time_cohorte(start_date, cohorte_duration)
+            festives_days = get_colombian_festivities(start_date,end_time)
+            print(f" Contidad de dias festivos: {festives_days}")
             print(end_time)
+            end_time += timedelta(days=festives_days)
             #return HttpResponseRedirect('result')
             return render(request, "register.html", {'end_time':end_time})
     else:
@@ -32,6 +36,18 @@ def time_cohorte(start_date, cohorte_duration):
     # Sumamos los valores para hacer el calculo
     future_date = start_date + timedelta(days=month_days_totales)
     return future_date
+
+#Crear la función que determine los días festivos en Colombia
+def get_colombian_festivities(start_date, end_date):
+    festivals = holidays.CO(years=2023)
+    #festivals_list = date(2023, 1, 1) in festivals
+    #count_festivals = len(festivals)
+    date_festivals = festivals.keys()
+    #festivals_list = [feast for feast in festivals if feast["date"].isocalendar()[0] == year][::-1]
+    segment_date = [fecha for fecha in date_festivals if start_date <= fecha <= end_date]
+    count_segment_date = len(segment_date)
+    return count_segment_date
+    
 
 # Función para mostrar el resultado en un template
 @require_POST
