58975934706c74a8996c02ee4cbdf1422ee6fc5b
Moises Ariza <arizamoises@gmail.com>
2023-07-26 22:18:55 -0500

    Create template and render response result month calculate

--- classtimeConversion/forms.py
+++ classtimeConversion/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    hours_total_number = forms.IntegerField(label="Número Total de horas")
-    hourly_intensity = forms.IntegerField(label="Intensidad horaria")
+    hours_total_number = forms.IntegerField(label="Número Total de horas del curso")
+    hourly_intensity = forms.IntegerField(label="Intensidad horaria semanal")
--- classtimeConversion/views.py
+++ classtimeConversion/views.py
@@ -3,6 +3,7 @@ from .forms import RegisterForm
 
 def months_calculation (request):
     hour_month_aprox = 30 * 24
+    month_week = 4
 
     if request.method == "POST":
         form = RegisterForm(data = request.POST)
@@ -13,16 +14,15 @@ def months_calculation (request):
             
             total_hours_course = form.cleaned_data['hours_total_number']
 
-            hourly_intensity = form.cleaned_data['hourly_intensity']
+            week_hourly_intensity = form.cleaned_data['hourly_intensity']
 
             #Realizamos sus respectivas operaciones
-            total_hours_delay = total_hours_course / hourly_intensity
-            print(total_hours_delay)
+            total_hours_delay = total_hours_course / week_hourly_intensity
 
-            month_delay_aprox = round(total_hours_delay / hour_month_aprox)
-            print(month_delay_aprox)
+            #month_delay_aprox = round(total_hours_delay / hour_month_aprox)
+            month_delay_aprox = round(total_hours_delay / month_week)
 
-            return render(request, 'month_calculate.html')
+            return render(request, 'month_calculate.html',{'month_delay_aprox':month_delay_aprox})
 
     else: 
         form = RegisterForm()
